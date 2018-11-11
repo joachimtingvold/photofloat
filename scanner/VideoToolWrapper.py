5cbc3f31e301c289b6aeaaf6d7d91ba894af4f3f
Joachim Tingvold <joachim@tingvold.com>
2018-11-11 04:22:15 +0100

    Use other timestamp for videos. Easier to manipulate timestamp via exiftool this way.

--- scanner/VideoToolWrapper.py
+++ scanner/VideoToolWrapper.py
@@ -6,15 +6,15 @@ class VideoToolWrapper(object):
 	def call(self, *args):
 		path = args[-1]
 		for tool in self.wrappers:
-			try:    
-                                if self.check_output:
-                                        p = subprocess.check_output((tool,) + args)
-                                else:
-                                        p = subprocess.call((tool,) + args)
-                                        if p > 0:
-                                                return False
-                                        else:
-                                                return "SUCCESS"
+			try:
+				if self.check_output:
+					p = subprocess.check_output((tool,) + args)
+				else:
+					p = subprocess.call((tool,) + args)
+					if p > 0:
+						return False
+					else:
+						return "SUCCESS"
 			except KeyboardInterrupt:
 				if self.cleanup:
 					self.remove(path)
@@ -37,11 +37,11 @@ class VideoToolWrapper(object):
 class VideoTranscodeWrapper(VideoToolWrapper):
 	def __init__(self):
 		self.wrappers = ['avconv', 'ffmpeg']
-                self.check_output = False
+		self.check_output = False
 		self.cleanup = True
 
 class VideoProbeWrapper(VideoToolWrapper):
 	def __init__(self):
 		self.wrappers = ['avprobe', 'ffprobe']
-                self.check_output = True
+		self.check_output = True
 		self.cleanup = False
